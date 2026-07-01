3e1d9800d1f09877381fe2313b26bd95d0d5fc21
GGAbhishekDhakal <gg.abhishekdhakal@gmail.com>
2026-07-01 09:22:32 +0545

    Fix API route ordering - move production catch-all after API routes

--- server/src/index.js
+++ server/src/index.js
@@ -15,13 +15,6 @@ const PORT = process.env.PORT || 3001;
 app.use(cors());
 app.use(express.json());
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../../client/dist')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
-  });
-}
-
 app.use('/api/exercises', exercisesRouter);
 app.use('/api/routines', routinesRouter);
 app.use('/api/logs', logsRouter);
@@ -42,6 +35,13 @@ app.get('/api/stats', (req, res) => {
     totalEntries: totalEntries.count,
   });
 });
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../../client/dist')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
