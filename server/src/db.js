38a9941461a3004e9df18b98983912adbd9ad726
GGAbhishekDhakal <gg.abhishekdhakal@gmail.com>
2026-07-01 11:33:43 +0545

    Use individual connection params to enforce family:4

--- server/src/db.js
+++ server/src/db.js
@@ -1,17 +1,18 @@
 const { Pool } = require('pg');
-const dns = require('dns');
 
 let pool;
 
 function getPool() {
   if (!pool) {
     const url = new URL(process.env.DATABASE_URL);
-    const addresses = dns.resolve4Sync(url.hostname);
-    url.hostname = addresses[0];
-
     pool = new Pool({
-      connectionString: url.toString(),
+      host: url.hostname,
+      port: parseInt(url.port || '5432'),
+      database: url.pathname.slice(1),
+      user: url.username,
+      password: url.password,
       ssl: { rejectUnauthorized: false },
+      family: 4,
     });
   }
   return pool;
